c7ca93a9396dd1b764dcab30a4130d02d741bd22
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-12-23 23:07:52 +0100

    credit cart for test payments

--- src/components/summary/summary.component.tsx
+++ src/components/summary/summary.component.tsx
@@ -44,6 +44,11 @@ const Summary: React.FC<SummaryProps> = ({ close, orderInfo }) => {
               </div>
             </div>
             <StripeCheckoutButton>pay</StripeCheckoutButton>
+            <p>
+              *Please use the following test credit card for payments*
+              <br />
+              4242 4242 4242 4242 - Exp: 12/21 - CVV: 123
+            </p>
           </div>
         );
       }}
